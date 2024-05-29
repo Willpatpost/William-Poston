6e9a7d0a8e3d29da081cf2218808a95e6948d41a
William Poston <146898928+Willpatpost@users.noreply.github.com>
2024-05-28 22:36:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -130,15 +130,6 @@ function checkWin() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('nav a').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            toggleSection(anchor.getAttribute('href').substring(1));
-        });
-    });
-    toggleSection('about'); // Automatically open the About section on load
-
-    // Add event listeners for project dropdowns
     document.querySelectorAll('.dropdown-btn').forEach(button => {
         button.addEventListener('click', function() {
             const projectId = this.getAttribute('data-target');
